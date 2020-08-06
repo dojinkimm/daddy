c6ce90d007ef1ddc16dc04701888a67235818bd2
dojin <dojinkim119@gmail.com>
2020-08-07 00:08:23 +0900

    enable randomizing values

--- random_word_to_excel.py
+++ random_word_to_excel.py
@@ -1,39 +1,17 @@
-from itertools import permutations
 import pandas as pd
-import PySimpleGUI as sg
+import re
+import random
 
-def arg_parse():
-    layout = [
-        [sg.Text('문장을 입력하세요', size=(25, 1))],
-        [sg.InputText()],
-        [sg.Text('저장할 파일의 이름을 입력하세요')],
-        [sg.InputText()],
-        [sg.Submit(), sg.Cancel()]]
+phrases = "something something"
+file_name = "test.csv"
 
-    window = sg.Window('문장 랜덤 생성기', layout)
+randomized_phrases = []
+for p in phrases:
+    delimiters = re.split(', |/', p)
+    combine = " ".join(delimiters)
+    arr = combine.split()
+    randomized_phrase = random.sample(arr, len(arr))
+    randomized_phrases.append(" ".join(randomized_phrase))
 
-    event, values = window.read()
-    window.close()
-
-    if event is None or event == 'Cancel':
-        exit()
-
-    return values
-
-
-args = arg_parse()
-
-phrase = args[0]
-file_name = args[1]+".csv"
-
-perm = permutations(phrase.split())
-arr = []
-
-for p in perm:
-    randomized_phraze = ""
-    for word in p:
-        randomized_phraze += word + " "
-    arr.append(randomized_phraze[:-1])
-
-df = pd.DataFrame(arr)
-df.to_csv(file_name)
+df = pd.DataFrame(randomized_phrases)
+df.to_csv(file_name, encoding="utf-8-sig")
---
+++ random_word_to_excel_gui.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import random
+import PySimpleGUI as sg
+import re
+
+def arg_parse():
+    layout = [
+        [sg.Text('문장을 입력하세요', size=(25, 1))],
+        [sg.InputText()],
+        [sg.Text('저장할 파일의 이름을 입력하세요')],
+        [sg.InputText()],
+        [sg.Submit(), sg.Cancel()]]
+
+    window = sg.Window('문장 랜덤 생성기', layout)
+
+    event, values = window.read()
+    window.close()
+
+    if event is None or event == 'Cancel':
+        exit()
+
+    return values
+
+
+args = arg_parse()
+
+phrases = args[0].split("\n")
+file_name = args[1]+".csv"
+if args[1] == "":
+    file_name = "test.csv"
+
+randomized_phrases = []
+for p in phrases:
+    delimiters = re.split(', |/', p)
+    combine = " ".join(delimiters)
+    arr = combine.split()
+    randomized_phrase = random.sample(arr, len(arr))
+    randomized_phrases.append(" ".join(randomized_phrase))
+
+df = pd.DataFrame(randomized_phrases)
+df.to_csv(file_name, encoding="utf-8-sig")
