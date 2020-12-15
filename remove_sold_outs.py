77e79324da3e7deb11d0a045d888e432a499d388
dojin <dojinkim119@gmail.com>
2020-12-15 21:25:26 +0900

    refactor removing sold out program

--- remove_sold_outs.py
+++ remove_sold_outs.py
@@ -1,9 +1,36 @@
 import csv
+
 import pandas as pd
+import PySimpleGUI as sg
+
+
+def arg_parse():
+    layout = [
+        [sg.Text("파일을 입력하세요(ex. test.csv)", size=(25, 1))],
+        [sg.InputText()],
+        [sg.Text("저장할 파일의 이름을 입력하세요")],
+        [sg.InputText()],
+        [sg.Submit(), sg.Cancel()],
+    ]
+
+    window = sg.Window("팔린 상품 제거기", layout)
+
+    event, values = window.read()
+    window.close()
+
+    if event is None or event == "Cancel":
+        exit()
+
+    return values
+
+
+args = arg_parse()
+read_file = args[0].split("\n")[0]
+write_file = args[1].split("\n")[0]
 
 past = dict()
 cur = dict()
-with open("sellcode_2020.csv", newline="") as csvfile:
+with open(read_file, newline="") as csvfile:
     rdr = csv.DictReader(csvfile)
     for i in rdr:
         past[i.get("code1")] = i.get("title1")
@@ -27,22 +54,49 @@ for k, v in cur.items():
     cur_list.append([k, v])
 
 arr = []
-for i in range(len(past_list)):
-    arr.append({
-        "code1": past_list[i][0],
-        "title1": past_list[i][1],
-        "code2": cur_list[i][0],
-        "title2": cur_list[i][1],
-    })
 
-arr.append({
-    "code1": "",
-    "title1": "",
-    "code2": cur_list[len(cur_list)-1][0],
-    "title2": cur_list[len(cur_list)-1][1],
-})
+if len(past_list) > len(cur_list):
+    for i in range(len(cur_list)):
+        arr.append(
+            {
+                "code1": past_list[i][0],
+                "title1": past_list[i][1],
+                "code2": cur_list[i][0],
+                "title2": cur_list[i][1],
+            }
+        )
 
-print(len(arr))
+    for j in range(len(cur_list), len(past_list)):
+        arr.append(
+            {
+                "code1": past_list[j][0],
+                "title1": past_list[j][1],
+                "code2": "",
+                "title2": "",
+            }
+        )
+
+
+if len(cur_list) > len(past_list):
+    for i in range(len(past_list)):
+        arr.append(
+            {
+                "code1": past_list[i][0],
+                "title1": past_list[i][1],
+                "code2": cur_list[i][0],
+                "title2": cur_list[i][1],
+            }
+        )
+
+    for j in range(len(past_list), len(cur_list)):
+        arr.append(
+            {
+                "code1": "",
+                "title1": "",
+                "code2": cur_list[j][0],
+                "title2": cur_list[j][1],
+            }
+        )
 
 df = pd.DataFrame(arr)
-df.to_csv("testtest.csv", encoding="utf-8-sig")
+df.to_csv(write_file, encoding="utf-8-sig")
